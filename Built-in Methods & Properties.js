84c9e5e3cf4466f727f033733192c477a7c8d5a6
790013438 <790013438@qq.com>
2018-12-13 14:46:50 +0800

    使用的参数下标超出实际调用的参数时会得到undefined

--- Built-in Methods & Properties.js
+++ Built-in Methods & Properties.js
@@ -8,8 +8,17 @@ message('Hi!', 10);
 //---------
 // 没有在function里定义参数，传来的多余参数也可以在function里面调用
 function message(message1) {
-   console.log(message1);
-   console.log(arguments[1]);
+  console.log(message1);
+  console.log(arguments[1]);
+}
+
+message('Hi!', 10);
+
+//---------
+// 没有在function里定义参数，使用的参数下标超出实际调用的参数时会得到undefined
+function message(message1) {
+  console.log(message1);
+  console.log(arguments[2]);
 }
 
 message('Hi!', 10);
