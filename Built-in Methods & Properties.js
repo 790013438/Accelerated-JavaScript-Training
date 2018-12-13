48ff8ad20ea13f312c54718b981bfae3d3c797b8
790013438 <790013438@qq.com>
2018-12-13 14:40:55 +0800

    没有在function里定义参数，传来的多余参数也可以在function里面调用

--- Built-in Methods & Properties.js
+++ Built-in Methods & Properties.js
@@ -4,3 +4,12 @@ function message(message1) {
 }
 
 message('Hi!', 10);
+
+//---------
+// 没有在function里定义参数，传来的多余参数也可以在function里面调用
+function message(message1) {
+   console.log(message1);
+   console.log(arguments[1]);
+}
+
+message('Hi!', 10);
