6376430d83f1393c5a0e7d2d811848a713bb119d
790013438 <790013438@qq.com>
2019-01-10 20:39:46 +0800

    连接字符串

--- String Functions.js
+++ String Functions.js
@@ -7,3 +7,7 @@ console.log(string[2]);
 
 // str.charAt(index) 得到同样的效果,按数组下标取字母
 console.log(string.charAt(2));
+
+// 连接字符串
+var string = 'Any text';
+console.log(string.concat(' add a new string'));
