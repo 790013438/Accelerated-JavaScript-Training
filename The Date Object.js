27c968de0a1083f235e9e4ff8392d7d630442917
790013438 <790013438@qq.com>
2019-01-24 10:01:40 +0800

    获得日期

---
+++ The Date Object.js
@@ -0,0 +1,12 @@
+// 获取当前的日期
+(function(){
+  var today = new Date();
+  console.log(today);
+})()
+
+// 获取指定的日期
+(function(){
+  var today = new Date(2016, 5, 26);
+  console.log(today);
+  console.log(today.toString());
+})()
