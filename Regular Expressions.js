c84910a9837d183487f99e69c983c398888b1b80
790013438 <790013438@qq.com>
2019-02-20 23:43:28 +0800

    pattern.exec-only-find-first-one

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -15,3 +15,21 @@
 
   console.log(pattern.exec(string));
 })()
+
+// 返回匹配的字符串
+(function() {
+  var string = 'abab';
+
+  var pattern = /ab/;
+
+  console.log(pattern.exec(string));
+})()
+
+// 返回true或者false
+(function() {
+    var string = 'abab';
+
+    var parttern = /ab/;
+
+    console.log(parttern.test(string));
+})()
