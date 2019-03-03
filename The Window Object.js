4d8ac9a7fda6476c2d0a47f4013a0717ef3dda3a
790013438 <790013438@qq.com>
2019-03-04 00:27:01 +0800

    将key和value存储在类似map的机制

--- The Window Object.js
+++ The Window Object.js
@@ -13,3 +13,9 @@ console.log(outerWidth)
   console.log(window.innerHeight);
   console.log(outerHeight);
 })()
+
+// 像map一样存储key 和 value，在内存中
+(function() {
+  console.log(localStorage.setItem('key1', 1000));
+  console.log(localStorage.getItem('key1'));
+})()
