cc2dfe9db06acc52533a90a0d3f8b8f18d11c902
790013438 <790013438@qq.com>
2019-04-25 23:12:45 +0800

    获取document的下一级

---
+++ The Document Object and How to Interact with It.js
@@ -0,0 +1,19 @@
+// 获得地址
+(function() {
+  console.log(document.URL);
+})()
+
+// 获得标题
+(function() {
+  console.log(document.title);
+})()
+
+// 获取身体
+(function() {
+  console.log(document.body);
+})()
+
+// 获取下一个节点
+(function() {
+  console.log(document.body.children)
+})()
