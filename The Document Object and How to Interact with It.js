5dbf269d400536fff483898cd435979d8941ce82
790013438 <790013438@qq.com>
2019-04-26 11:00:13 +0800

    修改背景颜色

--- The Document Object and How to Interact with It.js
+++ The Document Object and How to Interact with It.js
@@ -1,25 +1,31 @@
-// 获得地址
-(function() {
-  console.log(document.URL);
-})()
+function on() {
+  // 获得地址
+  (function() {
+    console.log(document.URL);
+  })()
 
-// 获得标题
-(function() {
-  console.log(document.title);
-})()
+  // 获得标题
+  (function() {
+    console.log(document.title);
+  })()
 
-// 获取身体
-(function() {
-  console.log(document.body);
-})()
+  // 获取身体
+  (function() {
+    console.log(document.body);
+  })()
 
-// 获取下一个节点
-(function() {
-  console.log(document.body.children)
-})()
+  // 获取下一个节点
+  (function() {
+    console.log(document.body.children)
+  })()
 
-// 选择包含的第一个节点
-(function() {
-  console.log(document.body.children[0])
-})()
+  // 选择包含的第一个节点
+  (function() {
+    console.log(document.body.children[0])
+  })()
 
+  (function(){
+    // 改变字体颜色
+    document.body.children[0].style.backgroundColor = 'red'
+  })()
+}
