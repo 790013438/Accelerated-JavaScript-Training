07869937377dca77b587aaf9079d24c9dcbd00ee
790013438 <790013438@qq.com>
2019-04-26 14:43:34 +0800

    获取第一个子节点的子节点（嵌套），的挨着节点

--- Traversing the DOM.js
+++ Traversing the DOM.js
@@ -1,4 +1,7 @@
 (function(){
   // 获取最后的子节点
   console.log(document.body.lastElementChild)
+
+  // 获取第一个子节点的子节点（嵌套），的挨着节点
+  console.log(document.body.firstElementChild.firstElementChild.nextElementSibling);
 })()
