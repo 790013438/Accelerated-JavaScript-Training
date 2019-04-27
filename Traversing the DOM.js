3f92a6e6a3564cf01fd478ff4584c6c85cafb904
790013438 <790013438@qq.com>
2019-04-27 23:05:01 +0800

    获取一个节点的父节点

--- Traversing the DOM.js
+++ Traversing the DOM.js
@@ -2,6 +2,9 @@
   // 获取最后的子节点
   console.log(document.body.lastElementChild)
 
-  // 获取第一个子节点的子节点（嵌套），的挨着节点
+  // 获取第一个子节点的子节点（嵌套），的挨着节点(向后)
   console.log(document.body.firstElementChild.firstElementChild.nextElementSibling);
+
+  // 获取一个节点的父节点
+  console.log(document.body.firstElementChild.firstElementChild.parentElement);
 })()
