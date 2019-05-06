236c1182eaf13c2390ee388e26a9530a933e9b48
790013438 <790013438@qq.com>
2019-05-06 23:14:05 +0800

    获取指定class的标签

--- Selecting Elements.js
+++ Selecting Elements.js
@@ -1,4 +1,8 @@
 (function(){
-  // 获取标签数组
+  // 获取标签li数组
   console.log(document.getElementsByTagName('li'));
+
+  // 获取到有className 为simple的标签
+  console.log(document.getElementsByClassName('simple'));
+
 })()
