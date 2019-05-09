2caed6809f64ef8350ab907600f187e684d285b6
790013438 <790013438@qq.com>
2019-05-09 17:22:22 +0800

    使用selector获取指定class的元素和获取指定属性的元素，和获取符合条件的所有元素

--- Selecting Elements with the Query Selector.js
+++ Selecting Elements with the Query Selector.js
@@ -1,3 +1,12 @@
 (function() {
   console.log(document.querySelector('h1'));
+
+  // 获取指定class的元素
+  console.log(document.querySelector('.simple'));
+
+  // 获得 href属性的元素 符合该选择的第一个元素
+  console.log(document.querySelector('[href]'));
+
+  // 获得选择的所有元素
+  console.log(document.querySelectorAll('[href]'));
 })()
