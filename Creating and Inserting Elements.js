34e1c2556a2e37fc89f502bd9cd33c83dfbc91f0
790013438 <790013438@qq.com>
2019-05-12 21:30:02 +0800

    在任意位置添加元素

--- Creating and Inserting Elements.js
+++ Creating and Inserting Elements.js
@@ -6,5 +6,14 @@
 
   var a = document.querySelector('a');
 
+  // 在元素内的最后添加
   a.appendChild(p);
+
+  var p1 = document.createElement('P');
+  p1.textContent = 'A new paragraph one!';
+  // 在指定位置添加，内部元素，
+  // p1 新node
+  // a  参考位置，node将添加到参考元素之前
+  a.parentNode.insertBefore(p1, a);
+
 })()
