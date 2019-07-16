1bfcbe1dbfbfdbf6cc624cb94ce976402273e093
790013438 <790013438@qq.com>
2019-07-16 21:43:12 +0800

    配置SpringBoot得到页面

---
+++ asynchronous/gs-rest-service/initial/src/main/java/io/github/manhattan/controller/EchartsController.java
@@ -0,0 +1,25 @@
+package io.github.manhattan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 此类的主要功能是： <br/>
+ * 1. 处理<br/>
+ * 此类提供的主要方法有：<br/>
+ * 1.
+ *
+ * @author chengmiao
+ * @version 1.0
+ * ClassName io.github.manhattan.controller
+ * Company: com.yinhai
+ * date 2019/7/16
+ * @see *#
+ */
+@Controller
+public class EchartsController {
+    @RequestMapping("/")
+    public String charts() {
+        return "/aa.html";
+    }
+}
---
+++ asynchronous/gs-rest-service/initial/src/main/java/io/github/manhattan/controller/IndexController.java
@@ -0,0 +1,17 @@
+package io.github.manhattan.controller;
+
+/**
+ * 此类的主要功能是： <br/>
+ * 1. 处理<br/>
+ * 此类提供的主要方法有：<br/>
+ * 1.
+ *
+ * @author chengmiao
+ * @version 1.0
+ * ClassName io.github.manhattan.controller
+ * Company: com.yinhai
+ * date 2019/7/16
+ * @see *#
+ */
+public class IndexController {
+}
